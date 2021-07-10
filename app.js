dafe99f1f44d1442a86e92984630e9a755a690ef
Katia Gibson <kcygibson@gmail.com>
2021-07-10 11:30:28 -0400

    generic js file template

---
+++ app.js
@@ -0,0 +1,15 @@
+/*-------------------------------- Constants --------------------------------*/
+//never gonna change...only appended to
+  /*---------------------------- Variables (state) ----------------------------*/
+  
+  //let colorArray; //for rotateColor
+  /*------------------------ Cached Element References ------------------------*/
+  //for event listeners mostly
+  
+  /*----------------------------- Event Listeners -----------------------------*/
+//for click events mostly 
+  /*-------------------------------- Functions --------------------------------*/
+  /* Examples:
+  init()
+  handleClick()
+  resetApp() */
